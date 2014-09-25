b49e98a2e1cf817c140e1f19a76a79434b8dafd9
Joel Ng <blood_elves88@yahoo.com.sg>
2014-09-25 17:36:17 +0800

    Commit eclipse project

---
+++ JOYTZ/src/Controller.java
@@ -0,0 +1,42 @@
+public class Controller {
+
+    private static String getInput() {
+        return GUI.getUserInput();
+    }
+    
+    private static void executeUserOutput(String outputCommand) {
+        if (outputCommand.equals("exit")) {     // TODO: This probably shouldn't be here (should be in executor)
+            System.exit(0);
+        }
+        GUI.displayOutput(outputCommand);
+    }
+    
+    public static void startController() {
+        String inputCommand;
+        String outputCommand;
+        String parsedCommand;
+        
+        outputCommand = getInput();              // TODO: Change outputText to inputCommand
+        // passToAnalyzer(inputCommand);
+        // parsedCommand = getFromAnalyzer();
+        // passToExecutor(parsedCommand);
+        // outputText = getFromExecutor();
+        executeUserOutput(outputCommand);
+    }
+    
+    public static void passToAnalyzer(String inputCommand) {
+        
+    }
+    
+    public static String getFromAnalyzer() {
+        return null;
+    }
+    
+    public static void passToExecutor(String command) {
+        
+    }
+    
+    public static String getFromExecutor() {
+        return null; 
+    }
+}
