e012ab358780954006d4b8eefdb2dd4255994513
rayzk <zk@Zhangs-MacBook-Pro-3.local>
2014-10-01 07:38:09 +0800

    information object

---
+++ JOYTZ/src/commandObject.java
@@ -0,0 +1,39 @@
+package mainComponent;
+
+public class commandObject {
+	String commandString;
+	Date time;
+	String location;
+	
+	/**
+	 * Constructor
+	 */
+	
+	commandObject(String userInput){
+		this.commandString = userInput;
+	}
+	
+	/**
+	 * methods
+	 */
+	
+	public String getCommandString(){
+		
+	}
+	
+	public Date getTime(){
+		
+	}
+	
+	public String getLocation(){
+		
+	}
+	
+	public void setTime(){
+		
+	}
+	
+	public void setLocation(){
+		
+	}
+}
---
+++ JOYTZ/src/returnObject.java
@@ -0,0 +1,12 @@
+package mainComponent;
+
+public class returnObject {
+	boolean result;
+	String showToUser;
+	
+	returnObject(boolean resu, String show){
+		this.result = resu;
+		this.showToUser = show;
+	}
+
+}
