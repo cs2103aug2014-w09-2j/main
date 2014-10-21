2aa9a935c03ff91cbe1be45b56f3ab0082119db1
WhyX <whyxce1994@gmail.com>
2014-10-22 03:26:29 +0800

    description added to every test cases in TestAnalyzer

--- JOYTZ/src/TestAnalyzer.java
+++ JOYTZ/src/TestAnalyzer.java
@@ -7,13 +7,9 @@ import org.junit.Test;
 
 public class TestAnalyzer {
 	private static final String ERROR_NULL_TASK_INDEX = "Task index is not inserted.\n";
-	private static final String ERROR_NULL_COMMAND = "Command is not inserted.\n";
 	private static final String ERROR_NULL_TASK = "Task name is not inserted.\n";
 	private static final String ERROR_NULL_INDICATOR = "Indicator is not inserted.\n";
-	private static final String ERROR_NULL_ARGUMENT = "Argument is not inserted.\n";
 	private static final String ERROR_INVALID_TASK_INDEX = "Task index indicated is invalid.\n";
-	private static final String ERROR_INVALID_INDICATOR = "Indicator is invalid.\n";
-	private static final String ERROR_INVALID_COMMAND = "Invalid command.\n";
 
 	private static Date d1 = new Date(114, 9, 15, -1, -1);
 	private static Date d2 = new Date(114, 9, 21, -1, -1);
@@ -54,57 +50,61 @@ public class TestAnalyzer {
 		expected2.setTaskStartTiming(String.valueOf(d3.getTime()));
 		expected2.setTaskEndTiming(String.valueOf(d4.getTime()));
 
-		// test case 1
+		// test case 1: test if the error catcher is working
 		assertEquals("null argument case is not handled",
 				expected.getErrorMessage(), Analyzer.runAnalyzer(test1)
 						.getErrorMessage());
 
-		// test case 2
+		// test case 2: test if the task name is able to be added
 		assertEquals("fail to get task name to be added",
 				expected.getTaskName(), Analyzer.runAnalyzer(test2)
 						.getTaskName());
 
-		// test case 3
+		// test case 3: test if the task description is able to be added
 		assertEquals("fail to get task description to be added",
 				expected.getTaskDescription(), Analyzer.runAnalyzer(test3)
 						.getTaskDescription());
 
-		// test case 4
+		// test case 4: test if the task start timing is able to be added
 		assertEquals("fail to get task start timing to be added",
 				expected.getTaskStartTiming(), Analyzer.runAnalyzer(test4)
 						.getTaskStartTiming());
 
-		// test case 5
+		// test case 5: test if the task location is able to be added
 		assertEquals("fail to get task location to be added",
 				expected.getTaskLocation(), Analyzer.runAnalyzer(test5)
 						.getTaskLocation());
 
-		// test case 6
+		// test case 6: test if the task priority is able to be added
 		assertEquals("fail to get task priority to be added",
 				expected.getTaskPriority(), Analyzer.runAnalyzer(test6)
 						.getTaskPriority());
 
-		// test case 7
+		// test case 7: test if the task end timing is able to be added
 		assertEquals("fail to get task end timing to be added",
 				expected.getTaskEndTiming(), Analyzer.runAnalyzer(test7)
 						.getTaskEndTiming());
 
-		// test case 8
+		// test case 8: test if the task start timing is able to be added
+		// correctly without the presence of task end timing
 		assertEquals("fail to get only task start timing to be added",
 				expected.getTaskStartTiming(), Analyzer.runAnalyzer(test8)
 						.getTaskStartTiming());
 
-		// test case 9
+		// test case 9: test if the task end timing is able to be added
+		// correctly without the presence of task start timing
 		assertEquals("fail to get only task end timing to be added",
 				expected.getTaskEndTiming(), Analyzer.runAnalyzer(test9)
 						.getTaskEndTiming());
 
-		// test case 10
+		// test case 10: test if the task start time will be recorded into task
+		// start timing correctly
 		assertEquals("fail to get task start timing (with time) to be added",
 				expected2.getTaskStartTiming(), Analyzer.runAnalyzer(test10)
 						.getTaskStartTiming());
 
-		// test case 11
+		// test case 11: test if the task end time will be recorded into task
+		// end timing correctly
 		assertEquals("fail to get task end timing (with time) to be added",
 				expected2.getTaskEndTiming(), Analyzer.runAnalyzer(test11)
 						.getTaskEndTiming());
@@ -126,17 +126,19 @@ public class TestAnalyzer {
 		ExecutableCommand expected3 = new ExecutableCommand("delete");
 		expected3.setErrorMessage(ERROR_INVALID_TASK_INDEX);
 
-		// test case 1
+		// test case 1: test if the error catcher for null argument is working
 		assertEquals("null argument case is not handled",
 				expected.getErrorMessage(), Analyzer.runAnalyzer(test1)
 						.getErrorMessage());
 
-		// test case 2
+		// test case 2: test if the error catcher for invalid task index is
+		// working
 		assertEquals("invalid task index case is not handled",
 				expected2.getErrorMessage(), Analyzer.runAnalyzer(test2)
 						.getErrorMessage());
 
-		// test case 3
+		// test case 3: test if the error catcher for invalid input argument is
+		// working
 		assertEquals("invalid input argument case is not handled",
 				expected3.getErrorMessage(), Analyzer.runAnalyzer(test3)
 						.getErrorMessage());
@@ -161,22 +163,24 @@ public class TestAnalyzer {
 		ExecutableCommand expected4 = new ExecutableCommand("update");
 		expected4.setKey("dating");
 
-		// test case 1
+		// test case 1: test if the error catcher for null argument is working
 		assertEquals("null argument case is not handled",
 				expected.getErrorMessage(), Analyzer.runAnalyzer(test1)
 						.getErrorMessage());
 
-		// test case 2
+		// test case 2: test if the error catcher for null update indicator is
+		// working
 		assertEquals("null update indicator case is not handled",
 				expected2.getErrorMessage(), Analyzer.runAnalyzer(test2)
 						.getErrorMessage());
 
-		// test case 3
+		// test case 3: test if the error catcher for invalid input argument is
+		// working
 		assertEquals("invalid input argument case is not handled",
 				expected3.getErrorMessage(), Analyzer.runAnalyzer(test3)
 						.getErrorMessage());
 
-		// test case 4
+		// test case 4: test if the task name to be updated is stored correctly
 		assertEquals("fail to get task name to be updated", expected4.getKey(),
 				Analyzer.runAnalyzer(test4).getKey());
 
