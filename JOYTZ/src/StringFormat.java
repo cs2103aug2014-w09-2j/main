c470a552289229438acfbc6cb9170a77bfb8e277
WhyX <whyxce1994@gmail.com>
2014-10-26 15:56:44 +0800

    constant String is extracted out and put into StringFormat class

--- JOYTZ/src/StringFormat.java
+++ JOYTZ/src/StringFormat.java
@@ -1,60 +1,42 @@
-
 public class StringFormat {
-	
+
 	/**
-	 * ###################################################
-	 * ##Conversion between Task Object and String. 	##
-	 * ###################################################
+	 * action
 	 */
-	// this is format for taskString.
-	public static String taskString = "%s-%s-%s-%s-%s";
-	public static int splitLimits = 3;
-	
-	
-	/**
-	 * ###################################################
-	 * ##action Name									##
-	 * ###################################################
-	 */
-	
 	public static final String ADD = "add";
 	public static final String DELETE = "delete";
 	public static final String DISPLAY = "display";
-	public static final String UPDATE = "update";	
-	
+	public static final String UPDATE = "update";
+	public static final String SEARCH = "search";
+	public static final String CLEAR = "clear";
+	public static final String SORT = "sort";
+	public static final String UNDO = "undo";
+	public static final String REDO = "redo";
+	public static final String EXIT = "exit";
+
 	/**
-	 * ###################################################
-	 * ##task attribute   		   						##
-	 * ##sort and search indicator						##
-	 * ###################################################
+	 * task attribute sort and search indicator
 	 */
-	
 	public static final String NAME = "name";
 	public static final String DESCRIPTION = "description";
 	public static final String START_TIMING = "start timing";
 	public static final String END_TIMING = "end timing";
 	public static final String LOCATION = "location";
 	public static final String PRIORITY = "priority";
-	
+
 	/**
-	 * ###################################################
-	 * ##update indicator  		   						##
-	 * ###################################################
+	 * update indicator
 	 */
-	
 	public static final String START_TIME = "start time";
 	public static final String END_TIME = "end time";
 	public static final String START_DATE = "start date";
 	public static final String END_DATE = "end date";
-	
+
 	/**
-	 * ###################################################
-	 * ##priority tag   		   						##
-	 * ###################################################
+	 * priority tag
 	 */
-	
 	public static final String HIGH_PRIORITY = "high";
 	public static final String MEDIUM_PRIORITY = "medium";
 	public static final String LOW_PRIORITY = "low";
-	
+
 }
