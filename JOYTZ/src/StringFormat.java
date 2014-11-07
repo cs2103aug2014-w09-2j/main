af9f295580970b61a6b8f8a0eae1d98af55d0a4b
WhyX <whyxce1994@gmail.com>
2014-11-07 20:50:17 +0800

    new test cases are added to testHandleUpdateCommand and relevant fixing actions are done to Analyzer

--- JOYTZ/src/StringFormat.java
+++ JOYTZ/src/StringFormat.java
@@ -1,12 +1,12 @@
 import java.text.SimpleDateFormat;
 
-
 public class StringFormat {
 
-	
-	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MM.dd HH:mm");
-	public static final SimpleDateFormat DATE_FORMAT_SHOWN_TO_USER = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm");
-	
+	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(
+			"yyyy.MM.dd HH:mm");
+	public static final SimpleDateFormat DATE_FORMAT_SHOWN_TO_USER = new SimpleDateFormat(
+			"yyyy.MM.dd 'at' HH:mm");
+
 	/**
 	 * action
 	 */
@@ -24,7 +24,7 @@ public class StringFormat {
 	public static final String HELP = "help";
 	public static final String TUTORIAL = "tutorial";
 	public static final String SETTINGS = "settings";
-	
+
 	/**
 	 * sort, search and update indicators
 	 */
@@ -42,7 +42,7 @@ public class StringFormat {
 	public static final String HIGH_PRIORITY = "high";
 	public static final String MEDIUM_PRIORITY = "medium";
 	public static final String LOW_PRIORITY = "low";
-	
+
 	public static final String INVALID_PRIORITY = "invalid";
 
 	/**
@@ -63,9 +63,22 @@ public class StringFormat {
 	public static final String FROM_INDICATOR = "from";
 	public static final String IMPORTANT = "important";
 	public static final String UNIMPORTANT = "unimportant";
-	
+
 	/**
 	 * invalid indicator
 	 */
 	public static final String INVALID = "invalid";
+
+	/**
+	 * check valid indicator
+	 */
+
+	public static boolean isValidIndicator(String indicator) {
+		return indicator.equals(StringFormat.NAME)
+				|| indicator.equals(StringFormat.DESCRIPTION)
+				|| indicator.equals(StringFormat.LOCATION)
+				|| indicator.equals(StringFormat.PRIORITY)
+				|| indicator.equals(StringFormat.START)
+				|| indicator.equals(StringFormat.END);
+	}
 }
