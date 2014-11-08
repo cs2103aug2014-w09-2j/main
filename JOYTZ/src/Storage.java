92ec99a3b2ff2954364227eadf4ec5876613aaec
rayzk <zhang2121619kai@gmail.com>
2014-11-08 22:46:46 +0800

    refactor update in storage

--- JOYTZ/src/Storage.java
+++ JOYTZ/src/Storage.java
@@ -113,8 +113,9 @@ public class Storage {
 	
 	public static boolean mark(int index) throws Exception{
 		if (index<0 || index >= displayTaskList.size()){
-			throw new Exception(String.format(ERROR_INVALID_TASK_INDEX, index));
+			throw new Exception(String.format(StringFormat.STR_ERROR_INVALID_TASK_INDEX, index));
 		}
+		
 		Task targetTask = displayTaskList.getTaskByIndex(index);
 		int targetTaskId = targetTask.getTaskId();
 		
@@ -138,10 +139,9 @@ public class Storage {
 	public static boolean update(int index, String updateIndicator, String updateKeyValue) throws Exception {
 		
 		if (index <= 0 || index > displayTaskList.size()) {
-			throw new Exception(String.format(ERROR_INVALID_TASK_INDEX, index));
+			throw new Exception(String.format(StringFormat.STR_ERROR_INVALID_TASK_INDEX, index));
 		}
 		
-		index--;
 		Task targetTask = displayTaskList.getTaskByIndex(index);
 		int targetTaskId = targetTask.getTaskId();
 		mainTaskList.deleteTaskById(targetTaskId);
