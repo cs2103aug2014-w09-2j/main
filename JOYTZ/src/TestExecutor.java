aa13f8876bc48ce7e298abef7ab7bcb24ec8d4f4
Thang <lecongthang.1102@gmail.com>
2014-11-10 00:43:30 +0800

    finish TestExecutor

--- JOYTZ/src/TestExecutor.java
+++ JOYTZ/src/TestExecutor.java
@@ -1,82 +1,172 @@
-/*
- * @author: Thang
- */
+// @author: A0112060E
 
 import static org.junit.Assert.*;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 public class TestExecutor {
-
 	ExecutableCommand obj;
+	Feedback fb;
 	ExecutableCommand clear = new ExecutableCommand("clear");
 
 	@Test
 	public void checkAddMethod() throws ParseException {
+		// check null object or not
+		assertNull("An object is not null", obj);
+
+		// check the wrong format or not
 		obj = new ExecutableCommand();
+		fb = new Feedback(false);
 
-		obj.setAction("add");
-		obj.setTaskName("Meeting");
-		obj.setTaskLocation("NUS");
-		obj.setTaskPriority("high");
-		obj.setTaskDescription("with teammates");
-
-		assertTrue("Fail to add", Executor.proceedAnalyzedCommand(obj)
-				.getResult());
+		obj.setAction("ADdd");
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertFalse("Correct input format", fb.getResult());
 		Executor.proceedAnalyzedCommand(clear);
 
-		/*
-		 * This is a boundary case for the non-empty ArrayList partition
-		 */
-		assertNotNull("No element in list", Executor
-				.proceedAnalyzedCommand(obj).getTaskStringList().size());
+		// check the input with all attributes correct or not
+		obj.setAction(StringFormat.ADD);
+		obj.setTaskName("buy bread");
+
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertTrue("Wrong input format", fb.getResult());
+
+		obj.setTaskDescription("delicious bread");
+		obj.setTaskLocation("KR MRT");
+		obj.setTaskPriority("important");
+		obj.setTaskStart("1424748600000");
+		obj.setTaskEnd("1424835900000");
+
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertTrue("Fail to add", fb.getResult());
 		Executor.proceedAnalyzedCommand(clear);
 
-		assertEquals("There's more than 1 element", 1, Executor
-				.proceedAnalyzedCommand(obj).getTaskStringList().size());
+		// check the input with the start date < the end date
+		obj.setTaskEnd("1424748000000");
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertFalse("Invalid start and end time", fb.getResult());
 		Executor.proceedAnalyzedCommand(clear);
 
-		assertNotEquals("There are 2 elements", 2, Executor
-				.proceedAnalyzedCommand(obj).getTaskStringList().size());
+		// check the input without task name
+		obj.setTaskName(null);
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertFalse("Invalid task name", fb.getResult());
+
 		Executor.proceedAnalyzedCommand(clear);
 	}
 
 	@Test
 	public void checkDeleteMethod() throws ParseException {
 		obj = new ExecutableCommand();
+		fb = new Feedback(false);
 
 		obj.setAction("add");
-		obj.setTaskName("meeting");
+		obj.setTaskName("meeting with friends");
 		Executor.proceedAnalyzedCommand(obj);
 
-		obj.setAction("add");
-		obj.setTaskName("eating");
+		obj.setTaskName("buy some apples");
 		Executor.proceedAnalyzedCommand(obj);
 
+		obj.setTaskName("buy car");
+		Executor.proceedAnalyzedCommand(obj);
+
+		obj.setTaskName("travelling");
+		Executor.proceedAnalyzedCommand(obj);
+
+		// check delete index is valid or not
+		fb = new Feedback(false);
+		obj.setAction("delete");
+		obj.setTaskId(10);
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertFalse("Valid index", fb.getResult());
+
+		// check delete function works or not
+		obj = new ExecutableCommand();
 		obj.setAction("delete");
 		obj.setTaskId(1);
+		obj.setTaskId(3);
+		fb = Executor.proceedAnalyzedCommand(obj);
 
-		/*
-		 * This is a boundary case for the non-empty ArrayList partition
-		 */
-
-		int size = Executor.proceedAnalyzedCommand(obj).getTaskStringList().size();
-
-		assertEquals("Fail to check size", 1, size);
-		assertNotEquals("Fail to check size", 2, size);
-
-		assertNotEquals("Fail to check List", 0, size);
-
-		/*
-		 * Delete the last item
-		 */
-		assertEquals("Fail to check List", 0, Executor.proceedAnalyzedCommand(obj)
+		assertTrue("Cannot delete 2 tasks", fb.getResult());
+		assertEquals("There are 2 tasks in the table", 2, fb
 				.getTaskStringList().size());
 
 		Executor.proceedAnalyzedCommand(clear);
 	}
-	
 
+	@Test
+	public void checkUpdateMethod() throws ParseException {
+		obj = new ExecutableCommand();
+		fb = new Feedback(false);
+
+		obj.setAction("add");
+		obj.setTaskName("buy car");
+		fb = Executor.proceedAnalyzedCommand(obj);
+
+		obj.setTaskName("buy bananas");
+		fb = Executor.proceedAnalyzedCommand(obj);
+
+		// check task Id
+		fb.setResult(false);
+		obj.setAction("update");
+		obj.setTaskId(2);
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertFalse("Valid index", fb.getResult());
+
+		// valid input
+		fb.setResult(false);
+		obj = new ExecutableCommand();
+		obj.setAction("update");
+		obj.setTaskId(1);
+		obj.setIndicator("priority");
+		obj.setKey("high");
+
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertTrue("Invalid indicator", fb.getResult());
+
+		Executor.proceedAnalyzedCommand(clear);
+	}
+
+	@Test
+	public void checkClearMethod() throws ParseException {
+		obj = new ExecutableCommand();
+		fb = new Feedback(false);
+
+		obj.setAction("add");
+		obj.setTaskName("abc");
+		fb = Executor.proceedAnalyzedCommand(obj);
+		assertEquals("More than 1 task", 1, fb.getTaskStringList().size());
+
+		fb.setResult(false);
+		fb = Executor.proceedAnalyzedCommand(clear);
+		assertEquals("Not empty", 0, fb.getTaskStringList().size());
+
+		Executor.proceedAnalyzedCommand(clear);
+	}
+
+	@Test
+	public void checkSearchMethod() throws ParseException {
+		obj = new ExecutableCommand();
+		fb = new Feedback(false);
+
+		obj.setAction("add");
+		obj.setTaskName("do assignments");
+		Executor.proceedAnalyzedCommand(obj);
+
+		obj.setTaskName("apply a part-time job");
+		Executor.proceedAnalyzedCommand(obj);
+
+		obj.setTaskName("apply scholarship");
+		Executor.proceedAnalyzedCommand(obj);
+
+		obj.setAction("search");
+		obj.setIndicator("name");
+		obj.setKey("apply");
+		fb = Executor.proceedAnalyzedCommand(obj);
+
+		assertEquals("There are not 2 tasks searched", 2, fb
+				.getTaskStringList().size());
+	}
 }
